130f22271038bb66e5736d21bbb14861d1ebf573
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-03-26 14:34:10 -0400

    center patch

--- config.h
+++ config.h
@@ -4,6 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const int user_bh = 2;             /* add an defined amount of pixels to the bar height */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
